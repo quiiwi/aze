a55c7068498661233050553dd203498c5880f7fc
Jean-Pierre .D <jean-pierre@iseabloom.com>
2018-12-18 11:05:42 +0100

    recapcha

--- src/Traits/ActionUserController.php
+++ src/Traits/ActionUserController.php
@@ -39,6 +39,10 @@ trait ActionUserController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
+            // recapcha
+            $recapcha = $request->request->get('g-recaptcha-response');
+
+            // password
             $password = $passwordEncoder->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
 
@@ -53,8 +57,16 @@ trait ActionUserController
 
             $user->setUsername($user->getEmail());
 
-            $em->persist($user);
-            $em->flush();
+            if($this->verifyRecapcha($recapcha) === true) {
+                $em->persist($user);
+                $em->flush();
+            } else {
+                $this->addFlash('error', "* Cochez la case recapcha");
+        
+                return $this->render('user/register.html.twig', [
+                    'form' => $form->createView()
+                ]);
+            }
 
             return $this->redirectToRoute('login');
         }
@@ -62,6 +74,22 @@ trait ActionUserController
         return $this->render('user/register.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    function verifyRecapcha($recaptcha){
+            $url = "https://www.google.com/recaptcha/api/siteverify";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+                "secret"=>"6LenyYIUAAAAALvItGZIHLUiW90IIVBt918vDcKU","response"=>$recaptcha));
+            $response = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($response);     
+        
+        return $data->success;        
     }
 
     /**
